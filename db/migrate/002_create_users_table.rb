e61e85d448a0d2c6f55defa5868352954eaa4f06
bl1ndy <ayoblindstone@gmail.com>
2022-12-19 17:14:34 +0300

    Refactor indents

--- db/migrate/002_create_users_table.rb
+++ db/migrate/002_create_users_table.rb
@@ -3,12 +3,12 @@
 Sequel.migration do
   change do
     create_table(:users) do
-      column :id, :uuid, null: false, default: Sequel.function(:uuid_generate_v4), primary_key: true
-      column :email, 'citext', null: false, unique: true
-      column :password_digest, String, null: false
+      column :id,                   :uuid, null: false, default: Sequel.function(:uuid_generate_v4), primary_key: true
+      column :email,                'citext', null: false, unique: true
+      column :password_digest,      String, null: false
       column :authentication_token, String, null: false, unique: true
-      column :created_at, DateTime, null: false, default: Sequel::CURRENT_TIMESTAMP
-      column :updated_at, DateTime, null: false, default: Sequel::CURRENT_TIMESTAMP
+      column :created_at,           DateTime, null: false, default: Sequel::CURRENT_TIMESTAMP
+      column :updated_at,           DateTime, null: false, default: Sequel::CURRENT_TIMESTAMP
     end
   end
 end
